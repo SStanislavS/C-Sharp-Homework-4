ac25e7708e595547e1f8bfe8b5789074e77e8356
Stanislav <S@mail.ru>
2022-12-14 13:49:09 +0300

    add Task 2

---
+++ Task 2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Программа принимает на входе число и выдает сумму цифр в числе.
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;    
+for (int i = 1; i <= n; i++)
+{
+    sum += n % 10; 
+    n /= 10;
+}
+Console.WriteLine("Сумма цифр введенного числа: " + sum);
